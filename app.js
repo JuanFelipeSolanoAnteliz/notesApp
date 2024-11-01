b7f49fb7c4c04c92a369256b6b192d9f6a791bd8
JuanFelipeSolanoAnteliz <juanfesolant0906@gmail.com>
2024-11-01 12:58:06 -0500

    chore: :truck: new enpoints  and some changes in the redirects urls

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 
 
 const corsOptions = {
-    origin: ['https://juanfelipesolanoanteliz.github.io', 'https://localhost:5000', "https://notes-app-delta-flame.vercel.app/"],
+    origin: ['https://juanfelipesolanoanteliz.github.io', 'https://localhost:5000', "https://notes-app-delta-flame.vercel.app"],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 };
